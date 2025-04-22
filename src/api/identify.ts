aec47cfe6aed33195e65feca5f7a9ed24ba43439
Thiemo Krause <thiemo.krause@dash0.com>
2025-04-22 13:54:43 +0200

    feat: add user.name to identify api

--- src/api/identify.ts
+++ src/api/identify.ts
@@ -1,8 +1,9 @@
 import { vars } from "../vars";
 import { addAttribute, removeAttribute } from "../utils";
-import { USER_EMAIL, USER_FULL_NAME, USER_ID } from "../semantic-conventions";
+import { USER_EMAIL, USER_FULL_NAME, USER_ID, USER_NAME } from "../semantic-conventions";
 
 type IdentifyOpts = {
+  name?: string;
   fullName?: string;
   email?: string;
 };
@@ -11,6 +12,11 @@ export function identify(id?: string, opts?: IdentifyOpts) {
   removeAttribute(vars.signalAttributes, USER_ID);
   if (id != null) {
     addAttribute(vars.signalAttributes, USER_ID, id);
+  }
+
+  removeAttribute(vars.signalAttributes, USER_NAME);
+  if (opts?.name != null) {
+    addAttribute(vars.signalAttributes, USER_NAME, opts.name);
   }
 
   removeAttribute(vars.signalAttributes, USER_FULL_NAME);
