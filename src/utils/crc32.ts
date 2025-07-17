d5a4a37146a4a07d403d2c6d1cc350390075f587
Matthias Rütten <matthias.ruetten@dash0.com>
2025-07-17 16:50:45 +0200

    move table generation out of the function

--- src/utils/crc32.ts
+++ src/utils/crc32.ts
@@ -1,19 +1,19 @@
 const POLYNOMIAL = 0xedb88320; // Polynomial used in CRC32 calculation
 
-export function crc32(str: string): number {
-  const table = new Uint32Array(256);
-  for (let i = 0; i < 256; i++) {
-    let c = i;
-    for (let k = 0; k < 8; k++) {
-      c = c & 1 ? POLYNOMIAL ^ (c >>> 1) : c >>> 1;
-    }
-    table[i] = c >>> 0;
+const TABLE = new Uint32Array(256);
+for (let i = 0; i < 256; i++) {
+  let c = i;
+  for (let k = 0; k < 8; k++) {
+    c = c & 1 ? POLYNOMIAL ^ (c >>> 1) : c >>> 1;
   }
+  TABLE[i] = c >>> 0;
+}
 
+export function crc32(str: string): number {
   let crc = 0xffffffff;
   for (let i = 0; i < str.length; i++) {
     const byte = str.charCodeAt(i);
-    crc = (crc >>> 8) ^ (table[(crc ^ byte) & 0xff] || 0);
+    crc = (crc >>> 8) ^ TABLE[(crc ^ byte) & 0xff]!;
   }
   return (crc ^ 0xffffffff) >>> 0;
 }
