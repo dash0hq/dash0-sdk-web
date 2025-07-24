d27fa214bc21790f5487331d8064b9f0e80d6602
Matthias Rütten <matthias.ruetten@dash0.com>
2025-07-24 09:30:04 +0200

    send the "dash0.web.event.id" attribute

--- src/semantic-conventions.ts
+++ src/semantic-conventions.ts
@@ -8,6 +8,7 @@ export const DEPLOYMENT_ID = "deployment.id";
 // Misc Signal Attribute Keys
 export const EVENT_NAME = "event.name";
 export const WEB_EVENT_TITLE = "dash0.web.event.title";
+export const WEB_EVENT_ID = "dash0.web.event.id";
 export const PAGE_LOAD_ID = "page.load.id";
 export const SESSION_ID = "session.id";
 export const USER_AGENT = "user_agent.original";
