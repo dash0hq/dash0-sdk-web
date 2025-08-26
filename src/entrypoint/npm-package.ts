6fef976bd9013babf1a22aeb7742d532f58bdb96
Thiemo Krause <thiemo.krause@dash0.com>
2025-08-26 09:34:43 +0200

    fix: add missing exports for utility types

--- src/entrypoint/npm-package.ts
+++ src/entrypoint/npm-package.ts
@@ -10,6 +10,12 @@ export * from "../api/log-level";
 export { terminateSession } from "../api/session";
 export { reportError } from "../api/report-error";
 
+// Additional utility types
+export type { AttributeValueType } from "../utils/otel";
+export type { AnyValue } from "../types/otlp";
+export type { PageViewMeta } from "../vars";
+export type { UrlAttributeScrubber, UrlAttributeRecord } from "../attributes/url";
+
 export function init(opts: InitOptions): void {
   debug(`${INIT_MESSAGE} (via package)`);
   initApi(opts);
